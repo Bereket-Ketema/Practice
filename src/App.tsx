64223241b8f88f9c57cfbe43cdc7a41ffc6e98f0
Bereket-Ketema <bereketketema866@gmail.com>
2026-06-26 16:02:27 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+      <div className="bg-white/10 backdrop-blur-md p-8 rounded-2xl shadow-2xl">
+        <h1 className="text-4xl font-bold text-white mb-4">
+          React 19 + Tailwind v4 🚀
+        </h1>
+        <p className="text-white/80">
+          Your bootcamp environment is ready!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+
+export default App
