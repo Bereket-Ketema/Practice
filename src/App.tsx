625055562db1a47148549819de115cbaf2988b9e
Bereket-Ketema <bereketketema866@gmail.com>
2026-07-09 12:02:18 +0300

    the main app have been cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,11 @@
 // import heroImg from './assets/hero.png'
 import './App.css'
 // App.tsx
-import React, { useState } from 'react';
-import TodoList from './component/Practice';
 
 function App() {
-  // Sample todo data
-  const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Learn TypeScript', completed: false },
-    { id: 3, text: 'Build a project', completed: true },
-  ]);
-
-  // Toggle function
-  const handleToggle = (id: number) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id
-          ? { ...todo, completed: !todo.completed }
-          : todo
-      )
-    );
-  };
-
   return (
-    <div>
-      <h1>Todo App</h1>
-      <TodoList todos={todos} onToggle={handleToggle} />
-    </div>
-  );
+    <div>Hellow world</div>
+  )
 }
 
 export default App;
